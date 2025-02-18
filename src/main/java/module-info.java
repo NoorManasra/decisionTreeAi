f20320d24bc72f170697d535279733ab34244093
NoorManasra <noor.manasra03@gmail.com>
2025-02-18 18:24:28 +0200

    done

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.demo5 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires weka;
+
+    opens org.example.demo5 to javafx.fxml;
+    exports org.example.demo5;
+}
